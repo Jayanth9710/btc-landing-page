20852c49cf21a626747a4a08c735e92e9b92a922
Jayanth9710 <kumarjayanth55@gmail.com>
2022-01-07 23:36:57 +0530

    add liquidity

--- src/Components/AddLiquidity/AddLiquidity.jsx
+++ src/Components/AddLiquidity/AddLiquidity.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import HelpIcon from '../HelpIcon/HelpIcon';
 import ButtonGroup from '../TradeLiquidityBtn/ButtonGroup';
 import './AddLiquidity.css';
+import {NavLink} from 'react-router-dom';
 
 function AddLiquidity() {
     return (
@@ -10,11 +11,13 @@ function AddLiquidity() {
             <div className="add__box">
             <div className="add__liquidity">
         <div className="liquidity__navbar">
+        <NavLink activeClassName='active-link' to='/liquidity' >
           <div className="liquidity__nav_1 col-2">
           <span className="back__icon">
               <i class="fas fa-chevron-left"></i>
             </span>
           </div>
+          </NavLink>
           <div className="liquidity__nav_2 col-6">
             <span className="liq_nav__title">Add Liquidity</span>
           </div>
@@ -33,9 +36,27 @@ function AddLiquidity() {
           <div className="icon__title">
             {/* <img className="inp__icon__1" src="bootomlogo.svg"></img> */}
             <span className="inp__title__1">Select a currency</span>
-            <span className="inp__choose__1">
+            <button data-bs-toggle="modal" data-bs-target="#exampleModal" className="inp__choose__1">
+              
               <i class="fas fa-chevron-down"></i>
-            </span>
+            </button>
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-scrollable">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
           </div>
           <input className="inp__1" type="text" placeholder="0.00" />
         </div>
--- src/Components/TradeLiquidity/LiquidityListing.jsx
+++ src/Components/TradeLiquidity/LiquidityListing.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import HelpIcon from "../HelpIcon/HelpIcon";
 import ButtonGroup from "../TradeLiquidityBtn/ButtonGroup";
 import "./LiquidityListing.css";
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 function LiquidityListing() {
   return (
@@ -27,7 +27,9 @@ function LiquidityListing() {
             </div>
           </div>
           <span className="tagline">Remove liquidity to receive tokens back</span>
+          <NavLink activeClassName='active-link' to='/add' >
           <button className="trade__wallet"><span className="add"><i class="fas fa-plus"></i></span>Add Liquidity</button>
+          </NavLink>
         </div>
       </div>
       </div>
